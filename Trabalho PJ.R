1ea28230c41209c4e6fbdb78404301883cdbc854
Vinicius Luis de Oliveira <137335142+Vinicius123i@users.noreply.github.com>
2026-05-30 15:54:34 -0300

    Add Multivariada Trabalho PJ.R

--- Trabalho PJ.R
+++ Trabalho PJ.R
@@ -135,3 +135,150 @@ rbind(
   Cenário7 = resumo(cen7),
   Cenário8 = resumo(cen8)
 )
+
+# Normal Multivariada
+
+library(MASS)
+
+mvnorm <- function(n_sim = 50000, mult_perdas = 1, cadm = 1,
+                                medias,
+                                Sigma){
+  dados <- mvrnorm(
+    n = n_sim,
+    mu = medias,
+    Sigma = Sigma
+  )
+  
+  colnames(dados) <- c(
+    "Remp",
+    "Cemp",
+    "Pauf",
+    "Rcap",
+    "Ccap",
+    "Rsv",
+    "Cadm"
+  )
+  
+  dados <- as.data.frame(dados)
+  
+  dados$Run <-
+    (dados$Remp - dados$Cemp - mult_perdas*dados$Pauf) +
+    (dados$Rcap - dados$Ccap) +
+    dados$Rsv -
+    cadm*dados$Cadm
+  
+  return(dados$Run)
+}
+
+dados5anos <- data.frame(
+  Remp = c(17240972.62, 22887819.44, 27154113.17, 28431184.15, 27837381.39),
+  Cemp = c(9881917.66, 13717674.33, 16529890.75, 17006139.08, 13422037.97),
+  Pauf = c(4273206.25, 1707172.27, 3413051.51, 4303517.55, 3118459.23),
+  Rcap = c(17082981.10, 21857139.60, 27286675.43, 29830049.65, 25437200.99),
+  Ccap = c(12144863.89, 16468313.68, 20952502.24, 22800071.35, 18767726.39),
+  Rsv  = c(4404808.64, 3621758.53, 4154535.69, 3922513.87, 5619494.85),
+  Cadm = c(5941205.94, 6970836.13, 6880157.87, 7426871.81, 7132300.12)
+)
+
+medias <- colMeans(dados5anos)
+
+Sigma <- cov(dados5anos)
+
+cena1 <- mvnorm(
+  n_sim = 50000,
+  medias = medias,
+  Sigma = Sigma
+)
+
+cena2 <- mvnorm(
+  n_sim = 50000,
+  medias = medias,
+  mult_perdas = 3,
+  Sigma = Sigma
+)
+
+cena3 <- mvnorm(
+  n_sim = 50000,
+  medias = medias,
+  cadm = 0.30,
+  Sigma = Sigma
+)
+
+cena4 <- mvnorm(
+  n_sim = 50000,
+  mult_perdas = 0.8, 
+  cadm = 0.75,
+  medias = medias,
+  Sigma = Sigma
+)
+
+hist(cena1/1e6, main="Cenário 1 Multivariada", col="lightblue", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia" ,xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+hist(cena2/1e6, main="Cenário 2 Multivariada", col="lightcoral", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia", xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+hist(cena3/1e6, main="Cenário 3 Multivariada", col="lightgreen", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia", xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+hist(cena4/1e6, main="Cenário 4 Multivariada", col="lightgoldenrod", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia", xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+
+
+rbind(
+  Cenário1 = resumo(cena1),
+  Cenário2 = resumo(cena2),
+  Cenário3 = resumo(cena3),
+  Cenário4 = resumo(cena4)
+)
+
+# Novos Cenários
+
+cena5 <- mvnorm(
+  n_sim = 50000,
+  cadm = 3,
+  medias = medias,
+  Sigma = Sigma
+)
+
+sigmac6 <- Sigma
+sigmac6[3,3] <- sigmac6[3,3]*2
+
+cena6 <- mvnorm(
+  n_sim = 50000,
+  mult_perdas = 2,
+  medias = medias,
+  Sigma = sigmac6
+)
+
+medc7 <- medias
+medc7[c(1,2)] <- medias[c(1,2)]/2
+
+cena7 <- mvnorm(
+  n_sim = 50000,
+  medias = medc7,
+  Sigma = Sigma
+)
+
+medc8 <- medias
+medc8[c(4,5)] <- medias[c(4,5)]*2
+
+cena8 <- mvnorm(
+  n_sim = 50000,
+  medias = medc8,
+  Sigma = Sigma
+)
+
+hist(cena5/1e6, main="Cenário 5 Multivariada", col="blue", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia" ,xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+hist(cena6/1e6, main="Cenário 6 Multivariada", col="red", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia", xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+hist(cena7/1e6, main="Cenário 7 Multivariada", col="green", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia", xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+hist(cena8/1e6, main="Cenário 8 Multivariada", col="gold", breaks=30, xlab = "Lucro Anual em Milhões", ylab = "Frênquecia", xlim = c(-30, 50), xaxt = "n")
+axis(1, at = seq(-20, 40, by = 10))
+
+rbind(
+  Cenário5 = resumo(cena5),
+  Cenário6 = resumo(cena6),
+  Cenário7 = resumo(cena7),
+  Cenário8 = resumo(cena8)
+)
+
